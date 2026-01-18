6a427bb22102ca3757efef3af591140bbcae5ea1
Yaswanth Reddy <143656322+YASWANTH1910@users.noreply.github.com>
2026-01-18 15:15:04 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
       <div className="logo">
         <img src="/images/one piece.png" alt="One Piece Logo" />
       </div>
 
-      <ul className="nav-links">
+      <div className={`menu-icon ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
         <li>
-          <Link to="/"><span>Home</span></Link>
+          <Link to="/" onClick={() => setIsOpen(false)}><span>Home</span></Link>
         </li>
         <li>
-          <Link to="/arcs"><span>Arcs</span></Link>
+          <Link to="/arcs" onClick={() => setIsOpen(false)}><span>Arcs</span></Link>
         </li>
         <li>
-          <Link to="/about"><span>About</span></Link>
+          <Link to="/about" onClick={() => setIsOpen(false)}><span>About</span></Link>
         </li>
       </ul>
     </div>
